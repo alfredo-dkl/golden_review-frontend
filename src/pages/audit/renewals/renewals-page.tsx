e717ccbe21947e8bde4031c5fe3c7e173b6f9e9b
Campione <80720402+alfrecampione@users.noreply.github.com>
2026-01-06 13:39:36 +0100

    Refactor Renewals and Unassigned Policies Pages to Remove Unused Components

--- src/pages/audit/renewals/renewals-page.tsx
+++ src/pages/audit/renewals/renewals-page.tsx
@@ -18,28 +18,3 @@ const RenewalsPage = () => {
 };
 
 export { RenewalsPage };
-
-                <CardTable>
-                    <ScrollArea>
-                        {isLoading ? (
-                            <div className="p-8 space-y-4">
-                                {[...Array(5)].map((_, i) => (
-                                    <Skeleton key={i} className="h-12 w-full" />
-                                ))}
-                            </div>
-                        ) : (
-                            <DataGridTable />
-                        )}
-                        <ScrollBar orientation="horizontal" />
-                    </ScrollArea>
-                </CardTable>
-
-                <CardFooter>
-                    <DataGridPagination sizes={[25, 50, 100]} />
-                </CardFooter>
-            </Card >
-        </DataGrid >
-    );
-};
-
-export { RenewalsPage };
--- src/pages/audit/unassigned/unassigned-policies-page.tsx
+++ src/pages/audit/unassigned/unassigned-policies-page.tsx
@@ -18,37 +18,3 @@ const UnassignedPoliciesPage = () => {
 };
 
 export { UnassignedPoliciesPage };
-className = "absolute end-1.5 top-1/2 -translate-y-1/2 h-6 w-6"
-onClick = {() => setSearchQuery('')}
-								>
-	<X />
-								</Button >
-							)}
-						</div >
-					</div >
-				</CardHeader >
-
-				<CardTable>
-					<ScrollArea>
-						{isLoading ? (
-							<div className="p-8 space-y-4">
-								{[...Array(5)].map((_, i) => (
-									<Skeleton key={i} className="h-12 w-full" />
-								))}
-							</div>
-						) : (
-							<DataGridTable />
-						)}
-						<ScrollBar orientation="horizontal" />
-					</ScrollArea>
-				</CardTable>
-
-				<CardFooter>
-					<DataGridPagination sizes={[25, 50, 100]} />
-				</CardFooter>
-			</Card >
-		</DataGrid >
-	);
-};
-
-export { UnassignedPoliciesPage };
